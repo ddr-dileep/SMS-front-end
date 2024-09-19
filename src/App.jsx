f77a8d2517db2ebc78fbbe44c9f6c6a10d0e5808
Dileep <ddr.dileepraloti@gmail.com>
2024-09-19 14:59:45 +0530

    feat : socket io

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import io from "socket.io-client";
+const END_POINT = "http://localhost:8080";
+var socket;
+
 const App = () => {
+  useEffect(() => {
+    socket = io(END_POINT);
+  }, []);
+
   return <div>App</div>;
 };
 
